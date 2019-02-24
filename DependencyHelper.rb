ded3be01a23f74ac0f0cca1688427cc433e543c4
Daniel <danieldossantos1707@yahoo.com.br>
2019-02-24 19:44:55 -0300

    Deleted request folder

--- DependencyHelper.rb
+++ DependencyHelper.rb
@@ -1,6 +1,5 @@
 Dir[File.join(File.dirname(__FILE__), './pages/*.rb')].each { |file| require file }
 
-require_relative 'lib/Request'
 require_relative 'resources/check/ValidFields'
 
 require 'rubygems' if RUBY_VERSION < '1.9'
